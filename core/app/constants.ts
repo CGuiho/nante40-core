7b63fa7e2e7de896629bb5c2fbbce06233b748b3
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2025-12-14 22:31:31 +0100

    feat: Initialize core application structure including PostgreSQL client, dependency management, environment configuration, and basic modules.

--- core/app/constants.ts
+++ core/app/constants.ts
@@ -12,16 +12,17 @@ import { Span } from '@guiho40/time'
  */
 
 export { APP_ID, APP_NAME, APP_ORIGIN, CORS_ALLOWED_ORIGINS, defaultCacheSpan, APP_DEFAULT_LANGUAGE }
-export type {  }
+export type {}
 
-const APP_ID = 'G0000' as const
-const APP_NAME = 'nante40-core' as const
-const APP_ORIGIN = 'https://core.guiho.co' as const
+const APP_ID = 'G1000' as const
+const APP_NAME = 'guiho-nante40-core' as const
+const APP_ORIGIN = 'https://core.nante40.guiho.co' as const
 
 const APP_DEFAULT_LANGUAGE: Language = 'en-US' as const
 
 const defaultCacheSpan = () => Span.minutes(random(10, 15))
 
+// prettier-ignore
 /**
  * CORS Allowed Origins
  * @warning Keep this list updated with all frontend applications domains.
@@ -30,40 +31,47 @@ const defaultCacheSpan = () => Span.minutes(random(10, 15))
  * @warning Do not append trailing slashes to the domains.
  */
 const CORS_ALLOWED_ORIGINS = [
-  'https://guiho.co',
-  'https://www.guiho.co',
+                      'https://guiho.co',
+                  'https://www.guiho.co',
 
-  'https://core.guiho.co',
-  'https://auth.guiho.co',
-  'https://account.guiho.co',
-  
-  'https://core.soneca40.guiho.co',
-  'https://soneca40.guiho.co',
+                 'https://core.guiho.co',
+                 'https://auth.guiho.co',
+              'https://account.guiho.co',
 
-  'https://core.kino40.guiho.co',
-  'https://kino40.guiho.co',
-  
-  'https://core.destro40.guiho.co',
-  'https://destro40.guiho.co',
-  
-  'https://core.turmab.guiho.co',
-  'https://turmab.guiho.co',
-  
-  'https://core.xawande40.guiho.co',
-  'https://xawande40.guiho.co',
-  
-  'https://core.cristo.guiho.co',
-  'https://cristo.guiho.co',
-  
-  'https://core.red-carpet.guiho.co',
-  'https://red-carpet.guiho.co',
-  
-  'https://core.liga40.guiho.co',
-  'https://liga40.guiho.co',
-  
-  'https://core.kitadi40.guiho.co',
-  'https://kitadi40.guiho.co',
-  
-  'https://core.banda40.guiho.co',
-  'https://banda40.guiho.co',
+             'https://razura40.guiho.co',
+        'https://core.razura40.guiho.co',
+
+              'https://nante40.guiho.co',
+         'https://core.nante40.guiho.co',
+
+             'https://soneca40.guiho.co',
+        'https://core.soneca40.guiho.co',
+
+               'https://kino40.guiho.co',
+          'https://core.kino40.guiho.co',
+
+             'https://destro40.guiho.co',
+        'https://core.destro40.guiho.co',
+
+               'https://turmab.guiho.co',
+          'https://core.turmab.guiho.co',
+
+            'https://xawande40.guiho.co',
+       'https://core.xawande40.guiho.co',
+
+               'https://cristo.guiho.co',
+          'https://core.cristo.guiho.co',
+
+           'https://red-carpet.guiho.co',
+      'https://core.red-carpet.guiho.co',
+
+               'https://liga40.guiho.co',
+          'https://core.liga40.guiho.co',
+
+             'https://kitadi40.guiho.co',
+        'https://core.kitadi40.guiho.co',
+
+              'https://banda40.guiho.co',
+         'https://core.banda40.guiho.co',
+
 ] as const
