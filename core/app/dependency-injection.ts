7e516093857a239fdfb6faa489ae73ff3bdd1bc7
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2025-12-14 22:32:42 +0100

    feat: Implement core application infrastructure including Elysia, dependency injection, database connections, and environment setup.

--- core/app/dependency-injection.ts
+++ core/app/dependency-injection.ts
@@ -6,6 +6,7 @@ import packageJson from '../package.json' assert { type: 'json' }
 import type { Secrets, Variables } from '#guiho/app/environment.js'
 import type { GuihoDatabase } from '@guiho40/guiho/server'
 import type { Logger } from '@guiho40/logger'
+import type { Nante40Database } from '@guiho40/nante40/server'
 
 export {}
 export type { DependencyInjection }
@@ -14,24 +15,33 @@ export type { DependencyInjection }
  * @section Dependency Injection
  */
 interface DependencyInjection {
+  packageJson: typeof packageJson
+
   logger: Logger
   secrets: Secrets
   variables: Variables
   /** Valkey instance. -- iovalkey, ioredis alternative. */
   valkey: Valkey
-  postgreSQL: Client
+  
+  guihoPostgreSQL: Client
+  nante40PostgreSQL: Client
 
   /**
-   * @alias guihoDb
-   * Nante40 Database instance.
+   * @alias nante40Db
+   * GUIHO Nante40 Database instance.
    * Drizzle ORM Database instance -- PostgreSQL under the hood
    */
-  db: GuihoDatabase
+  db: Nante40Database
+  
+  /**
+   * GUIHO Nante40 Database instance.
+   * Drizzle ORM Database instance -- PostgreSQL under the hood
+  */
+  nante40Db: Nante40Database
 
   /**
    * GUIHO Database instance.
    * Drizzle ORM Database instance -- PostgreSQL under the hood
    */
   guihoDb: GuihoDatabase
-  packageJson: typeof packageJson
 }
